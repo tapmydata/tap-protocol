3d943ab7186b139df814e9e3b87ac15685b4417e
Tom Holder <taholder@gmail.com>
2020-11-16 14:42:30 -0800

    Added support for pausing and unpausing with relevant tests.

--- contracts/TapToken.sol
+++ contracts/TapToken.sol
@@ -1,9 +1,37 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TapToken is ERC20 {
+contract TapToken is ERC20Pausable, Ownable {
     constructor(uint256 initialSupply) public ERC20("Tapmydata", "TAP") {
         _mint(msg.sender, initialSupply);
+    }
+
+    /**
+     * @dev Pauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_pause}.
+     *
+     * Requirements:
+     *
+     * - the caller must be contract owner
+     */
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_unpause}.
+     *
+     * Requirements:
+     *
+     * - the caller must be contract owner
+     */
+    function unpause() public onlyOwner {
+        _unpause();
     }
 }
--- test/TapToken.test.js
+++ test/TapToken.test.js
@@ -35,4 +35,58 @@ contract('Tap', function ([ owner, other ]) {
     // Store a value
     expect(await this.tap.symbol()).to.equal('TAP');
   });
+
+  it('can be paused', async function () {
+    await this.tap.transfer(other, 1000);
+    expect((await this.tap.balanceOf(other)).toString()).to.equal("1000");
+    
+    // Pause the contract
+    const web3Receipt = await this.tap.pause();
+    await expectEvent(
+      web3Receipt,
+      "Paused",
+      [ owner ]
+    );
+
+    await expectRevert(
+      this.tap.transfer(other, 1000),
+      "ERC20Pausable: token transfer while paused"
+    );
+
+    expect((await this.tap.balanceOf(other)).toString()).to.equal("1000");
+  });
+
+  it('can only be paused by owner', async function () {
+    await expectRevert(
+      this.tap.pause({from: other}),
+      "Ownable: caller is not the owner"
+    );
+  });
+
+  it('can be un-paused', async function () {
+    await this.tap.pause();
+
+    await expectRevert(
+      this.tap.transfer(other, 1000),
+      "ERC20Pausable: token transfer while paused"
+    );
+
+    const web3Receipt = await this.tap.unpause();
+    await expectEvent(
+      web3Receipt,
+      "Unpaused",
+      [ owner ]
+    );
+
+    await this.tap.transfer(other, 1000);
+    expect((await this.tap.balanceOf(other)).toString()).to.equal("1000");
+  });
+
+  it('can only be un-paused by owner', async function () {
+    await expectRevert(
+      this.tap.unpause({from: other}),
+      "Ownable: caller is not the owner"
+    );
+  });
+
 });
