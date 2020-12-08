db9abd9d8c9408749f6ee812c43fb4818724fbcc
Tom Holder <taholder@gmail.com>
2020-12-08 12:30:24 -0800

    Just to avoid repetition and make obvious these values are same.

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,11 +3,12 @@ require('dotenv').config()
 
 var Web3 = require('web3');
 const TapToken = artifacts.require("TapToken");
+const one_hundred_million = Web3.utils.toWei('100000000', 'ether');
 
 module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(TapToken, 'Tapmydata', 'TAP', Web3.utils.toWei('100000000', 'ether'));
+  await deployer.deploy(TapToken, 'Tapmydata', 'TAP', one_hundred_million);
   tapInstance = await TapToken.deployed();
   if(process.env.FROM) {
-    await tapInstance.mint(process.env.FROM, Web3.utils.toWei('100000000', 'ether'));
+    await tapInstance.mint(process.env.FROM, one_hundred_million);
   }
 };
