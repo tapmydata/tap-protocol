857bce40255fce53710366caf81b04d3ad757c6f
Tom Holder <taholder@gmail.com>
2020-12-08 12:28:39 -0800

    Config to deploy to mainnet (#6)
    * Config to deploy to mainnet
    
    * Wrap deployment min for tests that don't have env

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,7 +1,13 @@
 // migrations/2_deploy.js
+require('dotenv').config()
+
 var Web3 = require('web3');
 const TapToken = artifacts.require("TapToken");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(TapToken, 'Tapmydata', 'TAP', Web3.utils.toWei('100000000', 'ether'));
+  tapInstance = await TapToken.deployed();
+  if(process.env.FROM) {
+    await tapInstance.mint(process.env.FROM, Web3.utils.toWei('100000000', 'ether'));
+  }
 };
