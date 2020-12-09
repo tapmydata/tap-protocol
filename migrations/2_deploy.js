d2e925fd834f2305bada753da1434ee141fe7bd5
Tom Holder <taholder@gmail.com>
2020-12-09 13:23:12 -0800

    Remove merge markers

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -7,12 +7,7 @@ const TapToken = artifacts.require("TapToken");
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(TapToken, 'Tapmydata', 'TAP', Web3.utils.toWei('100000000', 'ether'));
   tapInstance = await TapToken.deployed();
-<<<<<<< Updated upstream
-  if(process.env.FROM) {
-    await tapInstance.mint(process.env.FROM, Web3.utils.toWei('100000000', 'ether'));
-=======
   if(process.env.MINT_TO) {
     await tapInstance.mint(process.env.MINT_TO, one_hundred_million);
->>>>>>> Stashed changes
   }
 };
