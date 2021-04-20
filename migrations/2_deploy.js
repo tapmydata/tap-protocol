9616c77b72b3d63fa397e238dfee74709302418d
Tom Holder <taholder@gmail.com>
2021-04-20 10:50:50 -0700

    Kovan config

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -6,7 +6,7 @@ const StakingRewardsFactory = artifacts.require("StakingRewardsFactory");
 
 module.exports = async function (deployer, network, accounts) {
 
-  await deployer.deploy(StakingRewardsFactory, '0x7f1f2d3dfa99678675ece1c243d3f7bc3746db5d', 1619827200);
+  await deployer.deploy(StakingRewardsFactory, '0x942684bDbd2d4897e766F383795cB94D00a0bCeC', 1618969745);
 
   //https://github.com/OpenZeppelin/openzeppelin-upgrades/blob/master/packages/plugin-truffle/README.md
 
@@ -14,8 +14,8 @@ module.exports = async function (deployer, network, accounts) {
 
   stakingRewardsFactoryInstance = await StakingRewardsFactory.deployed();
 
-  address1 = await stakingRewardsFactoryInstance.deploy('0x54049236fc1db3e274128176efedf7c69b4c6335', web3.utils.toWei('600000', 'ether'));
+  address1 = await stakingRewardsFactoryInstance.deploy('0x171496421fb12d061c996c038fb5e615374d38a4', web3.utils.toWei('600000', 'ether'));
 
-  address2 = await stakingRewardsFactoryInstance.deploy('0x3d52401f08dc655b7bbf468bf9f6bdee40c77a2b', web3.utils.toWei('600000', 'ether'));
+  address2 = await stakingRewardsFactoryInstance.deploy('0x8fdf59901082f61df9fecf4cff6d93df8c338aa7', web3.utils.toWei('600000', 'ether'));
 
 };
