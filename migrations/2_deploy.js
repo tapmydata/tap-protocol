9af2d7570d5a1c3d3a6b43cdfca6a838065a78d8
Tom Holder <taholder@gmail.com>
2021-07-28 11:14:45 -0700

    Update genesis date for v2

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -6,7 +6,7 @@ const StakingRewardsFactory = artifacts.require("StakingRewardsFactory");
 
 module.exports = async function (deployer, network, accounts) {
 
-  await deployer.deploy(StakingRewardsFactory, '0x7f1f2d3dfa99678675ece1c243d3f7bc3746db5d', 1619827200);
+  await deployer.deploy(StakingRewardsFactory, '0x7f1f2d3dfa99678675ece1c243d3f7bc3746db5d', 1627603200);
 
   //https://github.com/OpenZeppelin/openzeppelin-upgrades/blob/master/packages/plugin-truffle/README.md
 
@@ -16,6 +16,6 @@ module.exports = async function (deployer, network, accounts) {
 
   address1 = await stakingRewardsFactoryInstance.deploy('0x54049236fc1db3e274128176efedf7c69b4c6335', web3.utils.toWei('600000', 'ether'));
 
-  address2 = await stakingRewardsFactoryInstance.deploy('0x3d52401f08dc655b7bbf468bf9f6bdee40c77a2b', web3.utils.toWei('600000', 'ether'));
+  //address2 = await stakingRewardsFactoryInstance.deploy('0x3d52401f08dc655b7bbf468bf9f6bdee40c77a2b', web3.utils.toWei('600000', 'ether'));
 
 };
